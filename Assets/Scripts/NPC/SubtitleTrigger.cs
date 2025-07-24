8d220be8f2f8ecdaeaf6f03702ac06dd7c0a995d
KrystianSCH2003 <80778251+KrystianSCH2003@users.noreply.github.com>
2025-07-24 20:20:55 +0100

    added option for worldspace subs

--- Assets/Scripts/NPC/SubtitleTrigger.cs
+++ Assets/Scripts/NPC/SubtitleTrigger.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SubtitleTrigger : MonoBehaviour
 {
+    public enum SubtitleMode { ScreenSpace, WorldSpace }
+
     [System.Serializable]
     public class SubtitleLine
     {
@@ -15,17 +17,50 @@ public class SubtitleTrigger : MonoBehaviour
     [Header("Subtitle Lines")]
     public List<SubtitleLine> lines = new List<SubtitleLine>();
 
+    [Header("Subtitle Mode")]
+    public SubtitleMode subtitleMode = SubtitleMode.ScreenSpace;
+
+    [Header("World Space Settings")]
+    public GameObject worldSubtitlePrefab;
+    public Vector3 offset = new Vector3(0, 2f, 0);
+
     public void PlaySubtitles()
     {
-        if (SubtitleManager.Instance == null)
+        if (subtitleMode == SubtitleMode.ScreenSpace)
         {
-            Debug.LogWarning("No SubtitleManager found in scene!");
-            return;
-        }
+            if (SubtitleManager.Instance == null)
+            {
+                Debug.LogWarning("No SubtitleManager found in scene!");
+                return;
+            }
 
+            foreach (var line in lines)
+            {
+                SubtitleManager.Instance.ShowSubtitle(line.text, line.duration);
+            }
+        }
+        else if (subtitleMode == SubtitleMode.WorldSpace)
+        {
+            StartCoroutine(PlayWorldSubtitles());
+        }
+    }
+
+    private IEnumerator PlayWorldSubtitles()
+    {
         foreach (var line in lines)
         {
-            SubtitleManager.Instance.ShowSubtitle(line.text, line.duration);
+            GameObject instance = Instantiate(worldSubtitlePrefab, transform.position + offset, Quaternion.identity);
+
+            // Always face camera
+            instance.transform.LookAt(Camera.main.transform);
+            instance.transform.Rotate(0, 180, 0);
+
+            TMPro.TextMeshProUGUI tmp = instance.GetComponentInChildren<TMPro.TextMeshProUGUI>();
+            if (tmp != null)
+                tmp.text = line.text;
+
+            Destroy(instance, line.duration);
+            yield return new WaitForSeconds(line.duration);
         }
     }
 }
