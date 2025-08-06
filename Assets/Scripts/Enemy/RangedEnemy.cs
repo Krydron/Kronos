e8bfab11776aa34d4a03b6a0e3bf57258d9c26e3
KrystianSCH2003 <80778251+KrystianSCH2003@users.noreply.github.com>
2025-08-06 18:14:17 +0100

    added emitters to range

--- Assets/Scripts/Enemy/RangedEnemy.cs
+++ Assets/Scripts/Enemy/RangedEnemy.cs
@@ -1,18 +1,6 @@
-/**************************************************************************************************************
-* <Name> Class
-*
-* The header file for the <Name> class.
-* 
-* This class 
-* 
-*
-* Created by: <Kry> 
-* Date: <need to add>
-*
-***************************************************************************************************************/
 using System.Collections;
 using UnityEngine;
-using FMODUnity;  
+using FMODUnity;
 
 public class RangedEnemy : EnemyBase
 {
@@ -21,9 +9,9 @@ public class RangedEnemy : EnemyBase
     public float shootCooldown = 1.5f;
     private bool canShoot = true;
 
-    [Header("Shooting Audio")]
-    public EventReference shootSoundEvent;
-    public EventReference reloadSoundEvent;
+    [Header("Shooting Audio Emitters")]
+    public StudioEventEmitter shootEmitter;
+    public StudioEventEmitter reloadEmitter;
 
     protected override void Attack()
     {
@@ -44,15 +32,17 @@ public class RangedEnemy : EnemyBase
         canShoot = false;
         Debug.Log("Ranged enemy fires!");
 
+        // Aim at player
         firePoint.LookAt(new Vector3(player.transform.position.x, player.transform.position.y + 0.5f, player.transform.position.z));
 
-        // Play shooting sound
-        RuntimeManager.PlayOneShot(shootSoundEvent, transform.position);
+        // Play shooting sound from emitter
+        if (shootEmitter != null) shootEmitter.Play();
 
+        // Spawn bullet
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
 
-        // Play reload sound during cooldown
-        RuntimeManager.PlayOneShot(reloadSoundEvent, transform.position);
+        // Play reload sound from emitter
+        if (reloadEmitter != null) reloadEmitter.Play();
 
         yield return new WaitForSeconds(shootCooldown);
         canShoot = true;
